af8b36901889e84cce510a7ec157f4740312fad3
Adit <arnavle1998@gmail.com>
2023-09-04 22:29:44 +0530

    design enhancement

--- Task3/index.js
+++ Task3/index.js
@@ -8,12 +8,14 @@ Array.from(buttons).forEach((buttons) => {
             string = eval(string);
             document.querySelector("input").value=string;
         }else if(e.target.innerHTML == 'DEL'){
+            string = string.toString().slice(0, -1);
+            document.querySelector("input").value=string;
+        }else if(e.target.innerHTML == 'AC'){
             string = "";
             document.querySelector("input").value=string;
         }else{
-            console.log(e.target)
-        string = string + e.target.innerHTML;
-        document.querySelector("input").value=string;
+            string = string + e.target.innerHTML;
+            document.querySelector("input").value=string;
         }
         
     })
